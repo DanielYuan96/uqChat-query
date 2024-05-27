796f093f951e3228bfec68dee53987669e73e3b8
matrix <matrix@matrixs-MacBook-Pro.local>
2024-05-27 12:21:33 +0800

    test comit

--- src/main/java/com/example/uqchatquery/UqChatQueryApplication.java
+++ src/main/java/com/example/uqchatquery/UqChatQueryApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class UqChatQueryApplication {
 
     public static void main(String[] args) {
+        System.out.println("doing test");
         SpringApplication.run(UqChatQueryApplication.class, args);
     }
 
